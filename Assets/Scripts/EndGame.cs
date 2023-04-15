ce0454904a745eaf9a8f3fb2977b566c06e7f991
ngoc vo <ngocduyvo12@gmail.com>
2023-04-15 02:29:18 -0600

    first commit

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            text.text = "You Won!!!";
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpawnOnFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnFall : MonoBehaviour
+{
+    public GameObject initial;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -5)
+        {
+            transform.position = initial.transform.position;
+            transform.eulerAngles = initial.transform.eulerAngles;
+        }
+    }
+}
